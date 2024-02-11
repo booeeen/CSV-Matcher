ee1e0e852879ace02a3ae124b63f986dbc549110
ben.vielhaus <ben.vielhaus@gmail.com>
2024-02-11 18:15:34 +0100

    added script with logic

---
+++ skript.ps1
@@ -0,0 +1,25 @@
+$csv1Path = "..." # Path from 1st csv file
+$csv2Path = "..." # Path from 2nd csv file
+$neueCSVPath = "..." # Path where the data should be saved (csv file)
+
+$csvContent1 = Import-CSV -Path $csv1Path -Delimiter ";"
+$csvContent2 = Import-CSV -Path $csv2Path -Delimiter ";"
+
+$data1 = $csvContent1 | Select-Object Anzeigename
+$data2 = $csvContent2 | Select-Object Anzeigename
+$doubleCounter = 0
+
+foreach ($item1 in $data1) 
+{
+    foreach ($item2 in $data2) 
+    {
+        if ($item1.Anzeigename -eq $item2.Anzeigename) 
+        {
+            $item1 | Export-Csv -Path $neueCSVPath -Append -NoTypeInformation
+            $doubleCounter++
+        }
+
+    }
+}
+
+Write-Host("Duplicates successfully exported. Found $doubleCounter records.")
