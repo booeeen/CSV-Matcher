8e11bd680fdb2e582e42010b5c205c3858f297d9
ben.vielhaus <ben.vielhaus@gmail.com>
2024-02-17 14:02:56 +0100

    renamed var name

--- skript.ps1
+++ skript.ps1
@@ -1,6 +1,6 @@
 $csv1Path = "..." # Path from 1st csv file
 $csv2Path = "..." # Path from 2nd csv file
-$neueCSVPath = "..." # Path where the data should be saved (csv file)
+$filterdObjectsFile = "..." # Path where the data should be saved (csv file)
 
 $csvContent1 = Import-CSV -Path $csv1Path -Delimiter ";"
 $csvContent2 = Import-CSV -Path $csv2Path -Delimiter ";"
@@ -15,7 +15,7 @@ foreach ($item1 in $data1)
     {
         if ($item1.Anzeigename -eq $item2.Anzeigename) 
         {
-            $item1 | Export-Csv -Path $neueCSVPath -Append -NoTypeInformation
+            $item1 | Export-Csv -Path $filterdObjectsFile -Append -NoTypeInformation
             $doubleCounter++
         }
 
